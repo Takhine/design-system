156f6e5808f060724b450c7c6ce09773acd429ed
Aniketh Nair <anikethnair.developer@gmail.com>
2022-11-17 19:07:26 +0530

    snapshot tests

---
+++ packages/foundation/src/__test__/FontSize.test.ts
@@ -0,0 +1,5 @@
+import FontSize from '../FontSize';
+
+test('snapshop of fontsizes', ()=> {
+    expect(FontSize).toMatchSnapshot()
+})
---
+++ packages/foundation/src/__test__/Spacing.test.ts
@@ -0,0 +1,5 @@
+import Spacing from '../Spacing';
+
+test('snapshop of fontsizes', ()=> {
+    expect(Spacing).toMatchSnapshot()
+})
